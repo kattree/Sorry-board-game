317eb84197703731481ce719068eacf9dd7c8967
kattree <79332146+kattree@users.noreply.github.com>
2022-08-23 13:12:56 -0400

    Update main.cpp
    weee
    I can only work on this when I have extra time in my study hall class

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 //I'm really bad at coding but I really want to get better because I want to go into a programming career. 
-//last updated: 12/8 1:11pm
+// working on using functions but I still use the main because I've still got my training wheels on
+//last updated: 23/8 1:11pm
 
 
 #include <iostream>
@@ -26,20 +27,90 @@ int diceRoll(){
 return total;
 }
 
-void playerMovement(){
-  int player[getPlayer()];
+void playerMovement(int player,int roll){
+switch (roll){
+  case 2: {
+  printf("Moved forward two.");
+  player += 2;
+    break;
+}
+  case 3:{
+         printf("Moved forward three\n");
+        player += 3;
+
+        break;
+      }
+      case 4:{
+        printf("Moved back a space\n");
+        if (player >= 1){
+           player -= 1;
+        }
+      
+       break;
+      }
+      case 5:{
+        printf("Moved back three spaces\n");
+        if (player >=3) player-=3;
+          else player = 0;
+        
+        break;
+      }
+      case 6:{
+        printf("Moved forward six spaces\n");
+        player+=6;
+        
+        break;
+      }
+  case 7: break;
+
+   case 8:{
+        e++;
+        printf("Lost a turn!");
+        break;
+      }
+      case 9:{
+      
+        printf("Moved forward nine");
+        player+=9;
+        break;
+      }
+      case 10:{
+        
+        printf("Moved forward ten");
+        player+=10;
+        break;
+      }
+  case 11: break;
+    
+      case: 12 {
+        
+        printf("Sent back to the beginning!");
+        player=0;
+        break;
+      }
+  
+  
+}
+  
+  
   
 
   
 }
-void welcome(){
-  printf("Welcome to sorry! How many players are playing");
-  getPlayer();
-  int dplay[getPlayer()];
-  
-  
-  
-  
+int welcome(){
+  printf("Welcome to sorry! How many players are playing? 4 is the max");
+  int players = getPlayer();
+  //int dplay[getPlayer()];
+  while (players > 4 || players < 1) {
+    printf("hey stupid. I said only FOUR players! Now please reenter!");
+    players = getPlayer();}
+  printf("Confirm the amount of players is %d(y/n)",players);
+  char yn; cin>>yn;
+  if(yn == 'n'){
+    printf("Please reenter the amount of players you want");
+    players = getPlayer();}
+
+  return players;
 }
 
 
@@ -48,5 +119,14 @@ void welcome(){
 
 
 int main() {
-  std::cout << "Hello World!\n";
+  //std::cout << "Hello World!\n";
+  welcome();
+  int players;
 }
+
+
+/*
+Update log:
+Made the welcome message have a validation system for the amount of players
+Also started working on player movement(copied from the last project with changes)
+*/
